7f71a87ba70c1f617c60244171671ab00d54bbee
Masao Kitamura <masaok@gmail.com>
2023-09-13 23:48:34 -0700

    Initial Commit

---
+++ watchos-cloudkit-demo Watch App/watchos_cloudkit_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchos_cloudkit_demoApp.swift
+//  watchos-cloudkit-demo Watch App
+//
+//  Created by keckadmin on 9/13/23.
+//
+
+import SwiftUI
+
+@main
+struct watchos_cloudkit_demo_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ watchos-cloudkit-demo Watch AppUITests/watchos_cloudkit_demo_Watch_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  watchos_cloudkit_demo_Watch_AppUITestsLaunchTests.swift
+//  watchos-cloudkit-demo Watch AppUITests
+//
+//  Created by keckadmin on 9/13/23.
+//
+
+import XCTest
+
+final class watchos_cloudkit_demo_Watch_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
